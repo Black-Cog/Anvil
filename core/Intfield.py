c0a659621f23aaa3e808bca6a40d6fd2467306bb
Black-Cog <blackcogdigital@gmail.com>
2015-01-19 05:56:46 +0100

    Add Checkbox, Colorpicker, Dropmenu, Float, Int.

---
+++ core/Intfield.py
@@ -0,0 +1,11 @@
+
+from PySide import QtGui, QtCore
+
+class Intfield( QtGui.QSpinBox ):
+	"""docstring for Intfield"""
+	def __init__(self, value=1, min=0, max=2048, w=70, h=25, x=0, y=0 ):
+		super(Intfield, self).__init__()
+		self.setMinimum( min )
+		self.setMaximum( max )
+		self.setValue( value )
+		self.setGeometry( x, y, w, h )
