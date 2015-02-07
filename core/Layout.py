793a2ff6152ca48017572ef14f8484946cb51411
Black-Cog <blackcogdigital@gmail.com>
2015-02-07 10:13:02 +0100

    add scroll inside Layout
    add scroll inside Layout

--- core/Layout.py
+++ core/Layout.py
@@ -3,9 +3,21 @@ from PySide import QtCore, QtGui
 
 class Layout( QtGui.QWidget ):
 	"""docstring for Layout"""
-	def __init__( self, parent=None, x=None, y=None, w=None, h=None ):
+	def __init__( self, parent=None, x=None, y=None, w=None, h=None, scroll=False, margin=[10,10] ):
 		super( Layout, self ).__init__()
 		self.setParent( parent )
+		self.__scroll = scroll
+		self.__margin = margin
+
+		self.layout = self
+
+		if self.__scroll:
+			self.scrollArea = QtGui.QScrollArea()
+			self.scrollArea.setParent( self )
+			self.layout = QtGui.QWidget()
+			self.scrollArea.setWidget( self.layout )
+			# self.layout.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+			# self.layout.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
 
 		# define size
 		items  = [ x, y, w, h ]
@@ -16,17 +28,21 @@ class Layout( QtGui.QWidget ):
 			else : values.append( origin[i] )
 		self.setGeometry( values[0], values[1], values[2], values[3] )
 
+		if self.__scroll:
+			self.scrollArea.setGeometry( values[0], values[1], values[2]-20, values[3]-20 )
+
 		# init line offset vertical
-		self.linesH = 0
+		self.linesW = self.__margin[0]
+		self.linesH = self.__margin[1]
 
 	def add( self, added=None ):
 		try : iter( added )
 		except TypeError, te : added = [ added ]
 
-		offsetX = 0
+		offsetX = self.__margin[1]
 		offsetY = 0
 		for item in added :
-			item.setParent( self )
+			item.setParent( self.layout )
 			item.setVisible( True )
 
 			x = item.geometry().x()
@@ -41,6 +57,11 @@ class Layout( QtGui.QWidget ):
 
 			if offsetY < h : offsetY = h
 
-		# increment line offset vertical
+		# increment line offset height and width
 		self.linesH += offsetY + 5
+		if self.linesW < offsetX : self.linesW = offsetX
+
+		# resize teh lqyout in the case of a scroll
+		if self.__scroll : self.layout.resize( self.linesW+self.__margin[0], self.linesH+self.__margin[1] )
+
 
