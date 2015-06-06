2c7f06f7a78ce5f72f16fea3931f16a225b45735
Black-Cog <blackcogdigital@gmail.com>
2015-06-06 21:26:32 +0200

    Added MenuBar option to Window
    Added MenuBar option to Window

--- core/Window.py
+++ core/Window.py
@@ -3,15 +3,16 @@ from PySide import QtCore, QtGui
 
 class Window( QtGui.QMainWindow ):
 	"""docstring for Window"""
-	def __init__( self, title='undefind', size=[500, 400] ):
+	def __init__( self, title='undefind', size=[500, 400], menuBar=True ):
 		super( Window, self ).__init__()
 		self.setWindowTitle( title )
 		self.resize( size[0], size[1] )
 
-		menuBar    = QtGui.QMenuBar()
-		fileMenu   = QtGui.QMenu("File", self)
-		helpAction = fileMenu.addAction("Help")
-		exitAction = fileMenu.addAction("Exit")
+		if menuBar:
+			menuBar    = QtGui.QMenuBar()
+			fileMenu   = QtGui.QMenu("File", self)
+			helpAction = fileMenu.addAction("Help")
+			exitAction = fileMenu.addAction("Exit")
 
-		menuBar.addMenu( fileMenu )
-		self.setMenuBar( menuBar )	
+			menuBar.addMenu( fileMenu )
+			self.setMenuBar( menuBar )
