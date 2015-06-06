906893427edecbd57c8cd6adeb935ba0142973e7
Black-Cog <blackcogdigital@gmail.com>
2015-06-07 00:22:22 +0200

    Added Icon To Window.
    Added Icon To Window.

--- core/Window.py
+++ core/Window.py
@@ -3,10 +3,13 @@ from PySide import QtCore, QtGui
 
 class Window( QtGui.QMainWindow ):
 	"""docstring for Window"""
-	def __init__( self, title='undefind', size=[500, 400], menuBar=True ):
+	def __init__( self, title='undefind', iconPath=None, size=[500, 400], menuBar=True ):
 		super( Window, self ).__init__()
 		self.setWindowTitle( title )
 		self.resize( size[0], size[1] )
+
+		if iconPath:
+			self.setWindowIcon( QtGui.QIcon(iconPath) )
 
 		if menuBar:
 			menuBar    = QtGui.QMenuBar()
