5e8b39e00c5f36841a3a83d840ae4d18e84c9930
Black-Cog <blackcogdigital@gmail.com>
2015-06-12 12:43:20 +0200

    refactor Dropmenu
    refactor Dropmenu

--- core/Dropmenu.py
+++ core/Dropmenu.py
@@ -6,17 +6,33 @@ class Dropmenu( QtGui.QComboBox ):
 	def __init__( self, items=[], w=70, h=25, x=0, y=0 ):
 		super( Dropmenu, self ).__init__()
 		self.setGeometry( x, y, w, h )
+		self.__items = {}
 		self.add( items=items )
 
 	def value( self ):
 
 		return self.currentText()
 
+	def getData( self ):
+		currentText = self.currentText()
+		return currentText
+
+	def getValue( self ):
+		currentText = self.currentText()
+		
+		for key, value in self.__items.iteritems():
+			if value == currentText:
+				return key
+
 	def add(self, items=[], append=True, sort=True ):
 
 		# TODO add append and sort functions
-		self.addItems( items )
-
+		for item in items:
+			if isinstance( item, dict):
+				self.__items.update( item )				
+				if isinstance( item.values()[0], str):
+					self.addItems( [item.values()[0]] )
+					# print [item.values()[0]]
 	def remove(self, items=None, all=False ):
 
 		# TODO add check if items list
@@ -24,7 +40,10 @@ class Dropmenu( QtGui.QComboBox ):
 		for item in items:
 			self.removeItem( item )
 
-	def list( self ):
+	def list( self, data=False ):
 
-		# TODO add list fucntion
-		return None
+		if data:
+			return self.__items.values()
+
+		else:
+			return self.__items.keys()
